af85ebc36a8867cd6834e006af88396ad7499962
DKAIN-py <divyanshu06kain@gmail.com>
2026-08-22 17:22:01 +0000

    Repo Restructure

--- agents/sentiment_analysis/nlp_pipeline.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-# This executes the initial download and saves the model graph locally
-pipe = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
-pipe.save_pretrained("./local_roberta")
